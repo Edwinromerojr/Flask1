a6c3fbade81a8268ecfc512cb9effbc97a8e98db
Edwin Romero <edwinrmrjr@gmail.com>
2023-12-11 23:32:34 +0800

    home page

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 
-app = Flask(__main__)
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! this is a home page <H1>HELLO<H1>"
 
 
 if __name__ == "__main__":
