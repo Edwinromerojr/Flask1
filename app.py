b73684bcc20e8473b79247d91106d46616e87fd8
Edwin Romero <edwinrmrjr@gmail.com>
2023-12-14 23:17:08 +0800

    HTTP GET/POST

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -6,13 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template("index.html", content=["edwin", "popo", "wiwin"])
 
-@app.route("/<name>")
-def user(name):
-    return render_template("index.html", content=name, r=2)
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user", name="Admin!"))
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
