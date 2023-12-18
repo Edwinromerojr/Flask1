454d4785616417102516d552825293e6bc422107
Edwin Romero <edwinrmrjr@gmail.com>
2023-12-18 23:35:34 +0800

    crud

--- app.py
+++ app.py
@@ -22,7 +22,12 @@ class users(db.Model):
 
 @app.route("/")
 def home():
-    return render_template("index.html", content=["edwin", "popo", "wiwin"])
+    return render_template("index.html")
+
+@app.route("/view")
+def view():
+    return render_template("view.html", values=users.query.all())
+
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
@@ -30,6 +35,14 @@ def login():
         session.permanent = True
         user = request.form["nm"]
         session["user"] = user
+
+        found_user = users.query.filter_by(name=user).first()
+        if found_user:
+            session["email"] = found_user.email
+        else:
+            usr = users(user, "")
+            db.session.add(usr)
+            db.session.commit()
         flash("Login Succesful!")
         return redirect(url_for("user"))
     else:
@@ -47,6 +60,9 @@ def user():
         if request.method == "POST":
             email = request.form["email"]
             session["email"] = email
+            found_user = users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
             flash("Email was saved!")
         else:
             if "email" in session:
@@ -67,5 +83,6 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
