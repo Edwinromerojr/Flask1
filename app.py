01e9f5359894cc65b6bfef28ec706760b34c5e28
Edwin Romero <edwinrmrjr@gmail.com>
2023-12-20 23:22:15 +0800

    Blueprints & Using multiple python files

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
+from admin.second import second
 
 app = Flask(__name__)
+app.register_blueprint(second, url_prefix="/admin")
 app.secret_key = "hello"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
